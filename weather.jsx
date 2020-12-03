0f1199265de86504a92d8bbc790d971b7a807181
littlett98 <37678140+littlett98@users.noreply.github.com>
2020-12-03 14:39:27 -0500

    Added JSDocs

--- weather.jsx
+++ weather.jsx
@@ -1,33 +1,13 @@
 'use strict';
-/*
-class Timer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval(
-        this.tick.bind(this),
-        1000
-        );
-    }
-
-    tick() {
-        this.setState({date: new Date()})
-    }
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-      }
-
-    render() {
-        return (
-            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        );
-    }
-}
-*/
+/**
+ * This class creates everything for the weather component
+ * @author Trevor McCubbin
+ */
 class Weather extends React.Component {
+    /**
+     * Construtor method that sets the initial state and adds the input props into the component constructor
+     * @param {*} props the city you want to look up the weather for
+     */
     constructor(props) {
         super(props);
         this.state = {
@@ -36,6 +16,12 @@ class Weather extends React.Component {
             icon: null
         };
     }
+
+    /**
+     * When the weather component is mounted inside the app class this method is called.
+     * It fetches the weather for montreal and sends the responding json to the displayWeather method
+     * @throws Error if the fetch request is bad
+     */
     componentDidMount() {
         fetch('http://api.openweathermap.org/data/2.5/weather?q=' + this.props.city + '&appid=95c301e15b985b3e0264565d254ccf49&units=metric')
         .then(response => {
@@ -48,6 +34,10 @@ class Weather extends React.Component {
         .catch(error => console.error('There was a problem: '  + error));
     }
 
+    /**
+     * this method sets the state of our component to the current weather info
+     * @param {*} json The information returned by the fetch request
+     */
     displayWeather(json) {
         this.setState({
             temperature: json.main.temp,
@@ -55,7 +45,9 @@ class Weather extends React.Component {
             icon: json.weather[0].icon
         });
     }
-
+    /**
+     * This render the weather component
+     */
     render() {
         return (
             <section>
@@ -67,7 +59,14 @@ class Weather extends React.Component {
     }
 }
 
+/**
+ * This class is used to have a cleaner look at what is being rendered inside your webpage
+ * @author Trevor McCubbin
+ */
 class App extends React.Component {
+    /**
+     * Renders the weather component with montreal as the city props
+     */
     render() {
         return (
             <div>
@@ -77,4 +76,5 @@ class App extends React.Component {
     }
 }
 
+// render the app into the root div
 ReactDOM.render(<App/>, document.querySelector('#root'));
